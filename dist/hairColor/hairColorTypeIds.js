8bd8a24ed7a0e78ba61d487169dd8b8a15652994
ruslan.shukiurov <sukurovruslan68@gmail.com>
2020-11-13 20:08:25 +0200

    test

--- dist/hairColor/hairColorTypeIds.js
+++ dist/hairColor/hairColorTypeIds.js
@@ -1,14 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.LIGHT_BLOND = exports.DARK_BLOND = exports.AMBRE = exports.HIGHLIGHTS_DARK = exports.HIGHLIGHTS_LIGHT = exports.HIGHLIGHTS = exports.RED = exports.DARK = exports.LIGHT = void 0;
-exports.LIGHT = 1;
-exports.DARK = 2;
-exports.RED = 3;
-exports.HIGHLIGHTS = 4;
-exports.HIGHLIGHTS_LIGHT = 5;
-exports.HIGHLIGHTS_DARK = 6;
-exports.AMBRE = 7;
-exports.DARK_BLOND = 8;
-exports.LIGHT_BLOND = 9;
-console.log(exports.LIGHT_BLOND);
+var hairColorTypeIds = {
+    LIGHT: 1,
+    DARK: 2,
+    RED: 3,
+    HIGHLIGHTS: 4,
+    HIGHLIGHTS_LIGHT: 5,
+    HIGHLIGHTS_DARK: 6,
+    AMBRE: 7,
+    DARK_BLOND: 8,
+    LIGHT_BLOND: 9,
+};
+exports.default = hairColorTypeIds;
+// export const LIGHT = 1
+// export const DARK = 2
+// export const RED = 3
+// export const HIGHLIGHTS = 4
+// export const HIGHLIGHTS_LIGHT = 5
+// export const HIGHLIGHTS_DARK = 6
+// export const AMBRE = 7
+// export const DARK_BLOND = 8
+// export const LIGHT_BLOND = 9
+console.log(LIGHT_BLOND);
 //# sourceMappingURL=hairColorTypeIds.js.map
