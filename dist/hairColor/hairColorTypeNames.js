f3258fd0fb2ee910cc45961dfcf96007b388e210
ruslan.shukiurov <sukurovruslan68@gmail.com>
2020-12-24 12:48:43 +0200

    test

--- dist/hairColor/hairColorTypeNames.js
+++ dist/hairColor/hairColorTypeNames.js
@@ -3,14 +3,14 @@ var _a;
 exports.__esModule = true;
 var hairColorTypeIds_1 = require("./hairColorTypeIds");
 exports["default"] = (_a = {},
-    _a[hairColorTypeIds_1["default"].AMBRE] = 'Амбре',
-    _a[hairColorTypeIds_1["default"].DARK] = 'Темний',
-    _a[hairColorTypeIds_1["default"].HIGHLIGHTS_LIGHT] = 'Міліровка світла',
-    _a[hairColorTypeIds_1["default"].HIGHLIGHTS_DARK] = 'Міліровка темна',
-    _a[hairColorTypeIds_1["default"].HIGHLIGHTS] = 'Міліровка',
-    _a[hairColorTypeIds_1["default"].LIGHT] = 'Світлий',
-    _a[hairColorTypeIds_1["default"].RED] = 'Рудий',
-    _a[hairColorTypeIds_1["default"].LIGHT_BLOND] = 'Світло русий',
-    _a[hairColorTypeIds_1["default"].DARK_BLOND] = 'Темно русий',
+    _a[hairColorTypeIds_1.AMBRE] = 'Амбре',
+    _a[hairColorTypeIds_1.DARK] = 'Темний',
+    _a[hairColorTypeIds_1.HIGHLIGHTS_LIGHT] = 'Міліровка світла',
+    _a[hairColorTypeIds_1.HIGHLIGHTS_DARK] = 'Міліровка темна',
+    _a[hairColorTypeIds_1.HIGHLIGHTS] = 'Міліровка',
+    _a[hairColorTypeIds_1.LIGHT] = 'Світлий',
+    _a[hairColorTypeIds_1.RED] = 'Рудий',
+    _a[hairColorTypeIds_1.LIGHT_BLOND] = 'Світло русий',
+    _a[hairColorTypeIds_1.DARK_BLOND] = 'Темно русий',
     _a);
 //# sourceMappingURL=hairColorTypeNames.js.map
--- src/hairColor/hairColorTypeNames.ts
+++ src/hairColor/hairColorTypeNames.ts
@@ -1,13 +1,13 @@
-import hairColorTypeIds from './hairColorTypeIds';
+import { AMBRE, DARK, HIGHLIGHTS, HIGHLIGHTS_DARK, HIGHLIGHTS_LIGHT, LIGHT, RED, DARK_BLOND, LIGHT_BLOND } from './hairColorTypeIds';
 
 export default  {
-  [hairColorTypeIds.AMBRE]: 'Амбре',
-  [hairColorTypeIds.DARK]: 'Темний',
-  [hairColorTypeIds.HIGHLIGHTS_LIGHT]: 'Міліровка світла',
-  [hairColorTypeIds.HIGHLIGHTS_DARK]: 'Міліровка темна',
-  [hairColorTypeIds.HIGHLIGHTS]: 'Міліровка',
-  [hairColorTypeIds.LIGHT]: 'Світлий',
-  [hairColorTypeIds.RED]: 'Рудий',
-  [hairColorTypeIds.LIGHT_BLOND]: 'Світло русий',
-  [hairColorTypeIds.DARK_BLOND]: 'Темно русий',
+  [AMBRE]: 'Амбре',
+  [DARK]: 'Темний',
+  [HIGHLIGHTS_LIGHT]: 'Міліровка світла',
+  [HIGHLIGHTS_DARK]: 'Міліровка темна',
+  [HIGHLIGHTS]: 'Міліровка',
+  [LIGHT]: 'Світлий',
+  [RED]: 'Рудий',
+  [LIGHT_BLOND]: 'Світло русий',
+  [DARK_BLOND]: 'Темно русий',
 }
